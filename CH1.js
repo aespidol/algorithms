4c8e24ae7eb7909b02bedc109d2050ac0d9dc961
aespidol <aespidol@gmail.com>
2015-04-06 10:17:25 -0400

    Coding Dojo Algorithms

---
+++ CH1.js
@@ -0,0 +1,5 @@
+//(Print 1-255) Write a program that would print all the numbers from 1 to 255.
+for (var i=0; i<=255; i=i+1)
+  {
+    console.log(i);
+  }
---
+++ CH2.js
@@ -0,0 +1,5 @@
+//(Odd Numbers) Write a program that would print all the odd numbers from 1 to 1000.
+for(var i=1; i<1000; i = i+2)
+  {
+    console.log(i);
+  }
